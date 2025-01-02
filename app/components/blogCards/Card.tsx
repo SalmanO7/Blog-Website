928335505bcd12fb8fbb4c0bdb674c85a4176f64
SalmanO7 <msalman221020@gmail.com>
2025-01-02 05:17:47 +0500

    Complete Blog website

--- app/components/blogCards/Card.tsx
+++ app/components/blogCards/Card.tsx
@@ -6,31 +6,37 @@ import { blogsData } from './BlogData'
 
 const Card = () => {
     return (
-        <div
-            id='blogs'
-            className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-3 gap-y-8 px-6 sm:px-10 md:px-5 xl:px-10 2xl:px-14'
-        >{blogsData.map((item) => {
-            return <div key={item.id} className='shadow-lg rounded-2xl py-4 px-3 flex flex-col justify-center items-center '>
-                <div className='flex justify-center items-center py-3'>
-                    <Image
-                        className='h-44'
-                        src={item.image}
-                        alt={item.title} />
-                </div>
-                <div className='flex flex-col text-center'>
-                    <div className='text-sm xl:text-base py-2'>
-                        <Markdown>
-                            {item.title}
-                        </Markdown>
-                    </div>
-                    <Link href={`/${item.id}`}>
-                        <button
-                            className='text-sm font-semibold border bg-[#1c9b77] text-white transition transform hover:bg-gray-400 hover:shadow-lg rounded-lg py-1 px-3'
-                        >Read More</button>
-                    </Link>
-                </div>
+        <>
+            <div className='flex justify-center items-center flex-col py-10'>
+                <h1 className=' mt-7 font-semibold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>Blogs</h1>
+                <p className='text-gray-400 py-1'>Our Blogs</p>
             </div>
-        })}</div>
+            <div
+                id='blogs'
+                className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-3 gap-y-8 px-6 sm:px-10 md:px-5 xl:px-10 2xl:px-14'
+            >{blogsData.map((item) => {
+                return <div key={item.id} className='shadow-lg rounded-2xl py-4 px-3 flex flex-col justify-center items-center '>
+                    <div className='flex justify-center items-center py-3'>
+                        <Image
+                            className='h-44'
+                            src={item.image}
+                            alt={item.title} />
+                    </div>
+                    <div className='flex flex-col text-center'>
+                        <div className='text-sm xl:text-base py-2'>
+                            <Markdown>
+                                {item.title}
+                            </Markdown>
+                        </div>
+                        <Link href={`/${item.id}`}>
+                            <button
+                                className='text-sm font-semibold border bg-[#1c9b77] text-white transition transform hover:bg-gray-400 hover:shadow-lg rounded-lg py-1 px-3'
+                            >Read More</button>
+                        </Link>
+                    </div>
+                </div>
+            })}</div>
+        </>
     )
 }
 
--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,12 +1,20 @@
-import Image from 'next/image'
 import React from 'react'
-import HeroImg from "@/public/assets/Hero3.jpeg"
-
 
 const Hero = () => {
     return (
-        <div>
-            <Image className='w-full h-screen' src={HeroImg} alt={"hero Img"} />
+        <div style={{
+            backgroundImage: 'url("/assets/Hero3.jpg")',
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+        }}
+            className="h-[100vh] flex justify-center items-center ">
+            <div className="h-screen flex justify-center items-center">
+                <div className="text-center">
+                    <h1 className="px-5 text-[20px] md:text-3xl lg:text-6xl xl:text-7xl font-bold bg-white text-black rounded-full mx-10 py-1">Welcome to the Blogs</h1>
+                    <p className="mx-3 sm:mx-0 text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl text-white bg-black rounded-full sm:px-3 py-1 my-1">A place to share knowledge and better understand the world</p>
+                </div>
+            </div>
         </div>
     )
 }
